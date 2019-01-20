236c6a89f9cd6cc256198cf77021950d289bbbf1
iprometheus <to_sxl@qq.com>
2019-01-20 20:53:07 +0800

    堆内存溢出实例

---
+++ src/main/java/com/fangxing/javalearning/jvmlearning/outofmemory/heap/HeapTest.java
@@ -0,0 +1,22 @@
+package com.fangxing.javalearning.jvmlearning.outofmemory.heap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM参数： -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ */
+public class HeapTest {
+
+    static class OOMObject{}
+
+    public static void main(String[] args) {
+
+        List<OOMObject> list=new ArrayList<>();
+        while (true){
+            list.add(new OOMObject());
+        }
+
+    }
+
+}
