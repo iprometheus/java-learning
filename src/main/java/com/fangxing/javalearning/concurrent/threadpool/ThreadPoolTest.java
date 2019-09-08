dc3e93b2857195993118d0e2d2d26dc67a3d8856
iprometheus <to_sxl@qq.com>
2019-09-08 15:50:52 +0800

    threadpool

---
+++ src/main/java/com/fangxing/javalearning/concurrent/threadpool/ThreadPoolTest.java
@@ -0,0 +1,19 @@
+package com.fangxing.javalearning.concurrent.threadpool;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ThreadPoolTest {
+
+    public static void main(String[] args) {
+
+        //new ThreadPoolExecutor().submit();
+        //ThreadFactory
+        //RejectedExecutionHandler
+
+    }
+
+
+}
