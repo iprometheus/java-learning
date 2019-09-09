ed6dc82bce284b0531e5241ea21b5aeda74f5382
iprometheus <to_sxl@qq.com>
2019-09-09 23:06:41 +0800

    thread

---
+++ src/main/java/com/fangxing/javalearning/concurrent/executor/ExecutorTest.java
@@ -0,0 +1,25 @@
+package com.fangxing.javalearning.concurrent.executor;
+
+import java.util.concurrent.*;
+
+public class ExecutorTest {
+
+
+    public static void main(String[] args) {
+
+        //ScheduledThreadPoolExecutor;
+
+        //Future
+
+        //new ThreadPoolExecutor().submit()
+
+        //FutureTask
+
+        //new ThreadPoolExecutor().submit();
+
+        //Executors.newCachedThreadPool()
+
+        //Executors.callable()
+
+    }
+}
