5fa336f4f865bd410b073c3c28ee8dddb4480209
iprometheus <to_sxl@qq.com>
2019-09-10 23:56:32 +0800

    thread

---
+++ src/main/java/com/fangxing/javalearning/concurrent/executor/FixedThreadPoolTest.java
@@ -0,0 +1,11 @@
+package com.fangxing.javalearning.concurrent.executor;
+
+import java.util.concurrent.Executors;
+
+public class FixedThreadPoolTest {
+
+    public static void main(String[] args) {
+        //Executors.newSingleThreadExecutor()
+    }
+
+}
