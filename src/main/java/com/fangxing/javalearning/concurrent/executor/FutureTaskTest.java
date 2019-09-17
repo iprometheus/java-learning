3e20a935fe7341953d3bec9f6fbf08ce8dfeceb3
iprometheus <to_sxl@qq.com>
2019-09-17 23:50:25 +0800

    thread

---
+++ src/main/java/com/fangxing/javalearning/concurrent/executor/FutureTaskTest.java
@@ -0,0 +1,14 @@
+package com.fangxing.javalearning.concurrent.executor;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+public class FutureTaskTest {
+
+    public static void main(String[] args) {
+        //FutureTask
+        //Future
+        //AQS
+    }
+
+}
